d3aa6f87a80f82746c4aa73df686c8dc86a313bc
Marshall Farrier <info@marshallfarrier.com>
2013-12-06 09:48:57 -0800

    js game engine project link

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -6,6 +6,10 @@
             <p class="cm-margin-top-md">Contact:<br/>
             <a href="mailto:info@codemelon.com">info@codemelon.com</a></p>
             <h4>Projects</h4>
+            <ul>
+                <li><a href="https://github.com/aisthesis/codemelon-2014/tree/master/usr"
+                target="blank">JavaScript game engine</a></li>
+            </ul>
             
             <h4>Utilities</h4>
             <ul>
