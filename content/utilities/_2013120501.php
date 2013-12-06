9877d90297692eb6d5115d39208af6a1c00e5297
Marshall Farrier <info@marshallfarrier.com>
2013-12-05 17:52:50 -0800

    Add links to password management tools.

--- content/utilities/_2013120501.php
+++ content/utilities/_2013120501.php
@@ -13,7 +13,8 @@
 <div id="app">
     <div class="row">
         <div class="col-md-12">
-            <p>There are several excellent password utilities in circulation (LastPass, DashLane, etc.) that
+            <p>There are several excellent password utilities in circulation (<a href="https://lastpass.com/" 
+            target="blank">LastPass</a>, <a href="https://www.dashlane.com/en/" target="blank">DashLane</a>, etc.) that
             normally fully automate password creation as well as password use. But I occasionally run
             into situations where the automation doesn't work properly or a password with special characteristics
             is required. This utility is designed for such situations. It doesn't save anything, so you
