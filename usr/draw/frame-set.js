2936e289fad3bdfc7224ba37d2cfb0fe017a3939
Marshall Farrier <info@marshallfarrier.com>
2013-12-14 18:28:53 -0800

    Mechanism for offsetting the current frame.

--- usr/draw/frame-set.js
+++ usr/draw/frame-set.js
@@ -24,7 +24,14 @@ var _c = _c || {};
             // each frame is an object of type _c.draw.Image
             this.frames = params.frames;
             this.index = 0;
+            // temporary variable for holding the corner where the image will be drawn
+            this.corner = new _c.draw.Point(0, 0);
+            // offset to be applied to the frame's corner
+            this.offset = params.offset || new _c.draw.Vector(0, 0);
             this.loaded = _c.utils.fill(this.frames.length, false);
+            // the onload callback for each image should normally call 
+            // FrameSet#draw()
+            // The following ensures that all frames have been loaded
             this.frames.forEach(function(frame, i) {
                 frame.image.onload = function(event) {
                     _this.loaded[i] = true;
@@ -43,13 +50,15 @@ var _c = _c || {};
         draw: function(context) {
             var frame = this.frames[this.index];
 
+            frame.corner.offset(this.offset, this.corner);
             context.drawImage(frame.image,
-                frame.corner.x, frame.corner.y,
+                this.corner.x, this.corner.y,
                 frame.width, frame.height);
         },
 
         contains: function(point) {
-            return this.frames[this.index].contains(point);
+            return this.frames[this.index].contains(point.offset(this.offset, 
+                this.corner));
         }
     });
 })(_c);
