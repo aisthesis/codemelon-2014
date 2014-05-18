a4b00f6ce7cf0a4e9b625ae07155beae8c6b0e31
Marshall Farrier <info@marshallfarrier.com>
2014-05-18 00:45:48 -0700

    Blurb for Multi-tab Chrome extension.

--- content/blurbs/_2013110501.php
+++ content/blurbs/_2013110501.php
@@ -9,8 +9,9 @@
  */
 ?>
 <h2>Multi-tab Chrome extension <small><?php echo $date; ?></small></h2>
-<p>Describe...
-<?php
-echo "<a href='$details'>More</a>";
-?>
+<p>Clicking through the same links over and over again for daily viewing the charts of some stocks I follow, 
+I eventually decided that this would be a good task to automate but couldn't find a browser extension
+that allowed you to open a preset group of links with one click. So, I built a Chrome extension
+called <a href="https://chrome.google.com/webstore/detail/multi-tab/jifdlmkhpkkaeemdipgekomlmbkbmjjl"
+target="blank">Multi-tab</a> to do exactly that.
 </p>
