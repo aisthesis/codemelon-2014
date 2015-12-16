9f1dc1f34ed1708f65dc66fe15da2819360a8106
Marshall Farrier <info@marshallfarrier.com>
2015-12-15 18:17:21 -0800

    Market predictions.

--- content/details/_2015120401.php
+++ content/details/_2015120401.php
@@ -31,7 +31,7 @@
     <p>Note that while the model accurately identifies the crash
     of 2009 as a great time to buy, it would have been difficult
     in real time to recognize the exact best spot even with knowledge 
-    of these predictions. For example, toward the
+    only of predictions up to the current time. For example, toward the
     end of 2008, predicted growth spikes first to around 1.6, which translates
     to a predicted 60% return on investment over the prediction period.
     <em>In real time</em>, with knowledge only of events and metrics
@@ -39,8 +39,15 @@
     it would indeed have been a fairly good one. If you waited
     a little longer, however, until the stock went down another 50% to around $5 in early 2009,
     you would have done significantly better. So, while our crystal ball does 
-    provide some very useful information in this case, it is not obvious how best to make
-    use of the predictions <em>in real time</em> in an optimized investment strategy.<p>
+    provide some very useful information, it is not obvious how best to make
+    use of the predictions <em>in real time</em> in an optimized investment strategy.
+    Nor will I be discussing here how one might devise such a strategy. Here,
+    I'll only be interested in making good predictions and will take it for
+    granted that better predictions can be used for improving expected
+    profitability. I do want to point out that even given
+    relatively accurate albeit fallible predictions, it is by no means
+    obvious how to create an investment strategy that is optimal
+    in the sense of maximizing growth while minimizing risk.<p>
 
     <p>Since this is the first publication of results on a project we've
     been working on for a while now, let me back up and provide some context
@@ -87,15 +94,50 @@
     When you sell at suboptimal exit points, as you sometimes will, you miss out on market upturns
     where the buy-and-hold strategist still makes a profit.</p>
 
-    <h3>Measuring predictive accuracy</h3>
-    <p>baseline measure, standard error</p>
-    <h3>Testing on independent data</h3>
-    <p>Survivor bias</p>
+    <h3>Making accurate predictions</h3>
+    <p>Stock price behavior is influenced by many unpredictable factors and
+    thus includes an enormous amount of noise. Because we can never predict prices
+    exactly, we need a measure by which to judge which of several inexact
+    predictions is the most accurate. <a href="https://en.wikipedia.org/wiki/Standard_error"
+    target="_blank">Standard error</a> is the measure we are looking for to
+    quantify predictive accuracy.</p>
+    <p>The <em>standard error</em> of an estimate or prediction is the statistical
+    counterpart to <em>standard deviation</em>. Information on how to calculate it
+    is readily available and is outside the scope of this article. Its significance
+    can best be explained by example: If we say that our predictions have a standard
+    error of <code>0.5</code> relative to known actual values, that means that about 
+    <code>2/3</code> of our estimates will be within <code>0.5</code> of the actual value.</p>
+    
+    <p>The success of a machine learning algorithm is measured by low standard error <em>out
+    of sample</em>, that is on test data not visited by the algorithm while it was
+    learning the model. Machine learning algorithms are designed to minimize
+    error <em>in sample</em>, on the data set from which the model is developed.
+    So, they will invariably do well in sample but will not necessarily be
+    useful out of sample. What that means in our context is that we might 
+    develop a model from the last 10 years of data on some random selection
+    of stocks that makes very accurate predictions on the stocks used for learning.
+    But we could find that its predictions are not so accurate out of sample,
+    on stocks that were not used when the model was learned.</p>
+
+    <p>Our baseline for predicting growth was simply to use the mean, which can
+    be shown to be the constant model with the greatest accuracy. In other words,
+    our baseline was to take a random selection of equities and simply calculate 
+    their average growth over a given forecast interval. The standard error
+    of that prediction is then the value that needs to be beaten <em>out of sample</em>.
+    Given the noisy data, this task isn't particularly easy. Note also that
+    the baseline prediction can only translate into an investment strategy of 
+    <em>buy and hold</em>, as it is just using current price as the predictor
+    of future price. As is well known among specialists in quantitative finance, buy
+    and hold is not easy to beat.</p>
+
+    <p>baseline measure, standard error, out-of-sample, survivor bias</p>
     
     <img src="images/tsla_growth.png" class="img-responsive">
     <img src="images/ge_growth_detail.png" class="img-responsive">
 
     <h3>Technology choices</h3>
+    <p>Those more interested in the results than the technologies can
+    safely skip this section.</p>
     <p>Python, Mongo, pynance, pandas, numpy, matplotlib</p>
     <h3>Initial results</h3>
 </div>
