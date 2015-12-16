d1d25e6ae5768cb6ce5588963def8f3677283709
Marshall Farrier <info@marshallfarrier.com>
2015-12-16 14:10:03 -0800

    Update dates, more content.

---
+++ content/details/_2015121801.php
@@ -0,0 +1,7 @@
+<div class="page-header">
+    <h2>Machine learning technologies
+    <small><?php echo handle_to_date($_GET['id']); ?></small></h2>
+</div>
+<div class="media">
+    <p>Python, Mongo, pynance, pandas, numpy, matplotlib, deap</p>
+</div>
--- content/details/_2015123001.php
+++ content/details/_2015123001.php
@@ -11,11 +11,10 @@
     growth. After experimenting with a wide variety of metrics commonly used
     in quantitative finance, we have now developed an algorithm that yields
     statistically sound predictions. While I can't share the details
-    of our algorithms, I can describe what we're doing in general terms, discuss
-    some of our technology choices and share some visualizations 
-    of the results.</p>
+    of our algorithms, I can describe in general terms what we're trying to do
+    and share some visualizations of the results.</p>
 
-    <p>The following chart, for example, shows actual adjusted price (blue) compared to predicted 
+    <p>The following chart shows actual adjusted price (blue) compared to predicted 
     growth (red) for <em>GE</em> over the last 20 years. 
     <img src="images/ge_growth.png" class="img-responsive">
     <em>Adjusted</em> price is provided by <a href="http://finance.yahoo.com" 
@@ -49,13 +48,13 @@
     obvious how to create an investment strategy that is optimal
     in the sense of maximizing growth while minimizing risk.<p>
 
-    <p>Since this is the first publication of results on a project we've
-    been working on for a while now, let me back up and provide some context
-    on our project and what some of the considerations are that go into
-    the prediction component before discussing our technology choices
-    and initial results.</p>
+    <p>In order to understand what this chart <em>does</em> tell us,
+    let's look at the context of the problem we're attempting to solve and
+    the metrics by which to evaluate "better" or "worse" solutions. We can then
+    better understand what kind of information this chart is providing
+    as well as its limitations.</p>
 
-    <h3>Background</h3>
+    <h3>Context and problem</h3>
 
     <p>Our goal is to develop an automated investment strategy that can perform
     demonstrably better than investing in an index such as the S&P 500. "Better"
@@ -77,8 +76,8 @@
     seem like more often than not, the sell points were higher than
     the buy points. So, I backtested it on a small but
     random selection of stocks from the S&P over a 20 year period.
-    The algorithm did turn a profit, but one would
-    have made more money by just investing an equal share in each
+    The algorithm generated a profit on a statistically relevant set of data, but one would
+    have in fact made <em>more</em> money by just investing an equal share in each
     of the randomly selected stocks and selling at the end of the trial.
     In other words, buy and hold was superior to the algorithm.</p>
 
@@ -87,16 +86,29 @@
     the best buy and sell points, and then talk yourself into thinking
     there is a pattern that could have been spotted in advance.
     But if you try to quantify and backtest
-    such patterns <em>out of sample</em>, i.e., on data you haven't look at yet,
+    such patterns <em>out of sample</em>, i.e., on data you haven't looked at yet,
     you'll find that most of them don't hold up. They'll probably make
     some money because the market as a whole has advanced,
     but few algorithms can consistently beat buy and hold.
     When you sell at suboptimal exit points, as you sometimes will, you miss out on market upturns
     where the buy-and-hold strategist still makes a profit.</p>
 
+    <p>Our goal is to develop an algorithmic investment system
+    that performs demonstrably better than buy and hold. One component of this
+    system is making accurate growth predictions. It is this component that we are currently
+    working on and that the above chart illustrates. Our system is designed
+    to look for patterns over periods of several months and not for intraday patterns useful for day
+    trading. Most investors, including us, don't have the resources (such as fast fiber-optic
+    feeds in close proximity to the exchanges) to compete
+    with brokerages that receive market data within nanoseconds of its availability, so we have
+    focussed on medium- to long-term patterns where it doesn't matter so much if your
+    data feed is seconds or minutes behind the market. We're concerned with predicting
+    how well a stock is likely to perform in the next few months. Let's look now
+    at how to get a valid metric for this task.</p>
+
     <h3>Making accurate predictions</h3>
     <p>Stock price behavior is influenced by many unpredictable factors and
-    thus includes an enormous amount of noise. Because we can never predict prices
+    thus includes an enormous amount of noise. Because we can never predict future prices
     exactly, we need a measure by which to judge which of several inexact
     predictions is the most accurate. <a href="https://en.wikipedia.org/wiki/Standard_error"
     target="_blank">Standard error</a> is the measure we are looking for to
@@ -109,35 +121,39 @@
     <code>2/3</code> of our estimates will be within <code>0.5</code> of the actual value.</p>
     
     <p>The success of a machine learning algorithm is measured by low standard error <em>out
-    of sample</em>, that is on test data not visited by the algorithm while it was
-    learning the model. Machine learning algorithms are designed to minimize
-    error <em>in sample</em>, on the data set from which the model is developed.
+    of sample</em>--i.e., on test data not visited by the algorithm while it was
+    learning the model. Machine learning algorithms are <em>designed</em> to minimize
+    error <em>in sample</em>, on the data from which the model is developed.
     So, they will invariably do well in sample but will not necessarily be
     useful out of sample. What that means in our context is that we might 
-    develop a model from the last 10 years of data on some random selection
-    of stocks that makes very accurate predictions on the stocks used for learning.
-    But we could find that its predictions are not so accurate out of sample,
-    on stocks that were not used when the model was learned.</p>
+    develop a model from past data on some random selection
+    of stocks that makes very accurate predictions on the stocks  and time period used for learning.
+    But we could find that its predictions are not accurate out of sample,
+    on stocks that were not used in developing the model.</p>
 
     <p>Our baseline for predicting growth was simply to use the mean, which can
     be shown to be the constant model with the greatest accuracy. In other words,
     our baseline was to take a random selection of equities and simply calculate 
     their average growth over a given forecast interval. The standard error
-    of that prediction is then the value that needs to be beaten <em>out of sample</em>.
-    Given the noisy data, this task isn't particularly easy. Note also that
-    the baseline prediction can only translate into an investment strategy of 
-    <em>buy and hold</em>, as it is just using current price as the predictor
-    of future price. As is well known among specialists in quantitative finance, buy
-    and hold is not easy to beat.</p>
-
-    <p>baseline measure, standard error, out-of-sample, survivor bias</p>
+    of this constant prediction is then the value that needs to be beaten <em>out of sample</em>.
+    Given the noisy data, this task isn't easy. As the basis for an investment strategy, 
+    this baseline prediction will invariably lead to  
+    <em>buy and hold</em>. The baseline prediction is in fact impossible to beat if
+    it is true that current price alone is the best predictor of future price.
+    As is well known among specialists in quantitative finance, buy
+    and hold is not easy to beat. Many experts believe that 
+    efficient markets price in all available information. If their hypothesis 
+    holds, current price 
+    <em>is</em> in fact the best predictor of future price.</p>
+    <p>We have experimented with numerous technical
+    indicators to determine which, if any, could lead to models consistently showing lower out-of-sample
+    error than the baseline prediction, and we have arrived at features that
+    have shown modest but consistent improvements relative to 
+    the baseline. While I can't share what these features are, I can present
+    some charts illustrating what the predictions are telling us.</p>
     
-    <img src="images/tsla_growth.png" class="img-responsive">
-    <img src="images/ge_growth_detail.png" class="img-responsive">
-
-    <h3>Technology choices</h3>
-    <p>Those more interested in the results than the technologies can
-    safely skip this section.</p>
-    <p>Python, Mongo, pynance, pandas, numpy, matplotlib</p>
     <h3>Initial results</h3>
+    <img src="images/ge_growth_detail.png" class="img-responsive">
+    <img src="images/tsla_growth.png" class="img-responsive">
+
 </div>
